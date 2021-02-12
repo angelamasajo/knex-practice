2dca28c21ed4ffc519dc71d39c578d5325fb939d
Angela Masajo <aabmasajo@gmail.com>
2021-02-12 17:28:12 -0600

    shopping list service and articles service

---
+++ blogful-scripts/seed.blogful.sql
@@ -0,0 +1,24 @@
+INSERT INTO blogful_articles (title, content, date_published)
+VALUES
+    ('Hello1', 'Let''s talk about some fun greetings ok!!!!', now()),
+    ('Northeast1', 'Why can''t we just pick one', now() - '6 days'::INTERVAL),
+    ('hi1', 'sdfadfa', now()),
+    ('Joji1', 'What a good artist', now()),
+    ('Hello2', 'Let''s talk about some fun greetings ok!!!!', now()),
+    ('Northeast2', 'Why can''t we just pick one', now()),
+    ('hi2', 'sdfadfa', now() - '2 days'::INTERVAL),
+    ('Joji2', 'What a good artist', now()),
+    ('Hello3', 'Let''s talk about some fun greetings ok!!!!', now()),
+    ('Northeast3', 'Why can''t we just pick one', now()),
+    ('hi3', 'sdfadfa', now()),
+    ('Joji3', 'What a good artist', now() - '7 days'::INTERVAL),
+    ('Hello4', 'Let''s talk about some fun greetings ok!!!!', now()),
+    ('Northeast4', 'Why can''t we just pick one', now()),
+    ('hi4', 'sdfadfa', now()),
+    ('Joji4', 'What a good artist', now()),
+    ('Hello5', 'Let''s talk about some fun greetings ok!!!!', now() - '11 days'::INTERVAL),
+    ('Northeast5', 'Why can''t we just pick one', now()),
+    ('hi5', 'sdfadfa', now()),
+    ('Joji5', 'What a good artist', now());
+
+
